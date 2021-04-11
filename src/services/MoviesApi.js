a270b79c84ef88a4093d31df9aec5e876d444555
Anton Yarosh <on.yaroshanton@gmail.com>
2021-04-11 12:51:21 +0300

    last

--- src/services/MoviesApi.js
+++ src/services/MoviesApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 export function fetchTrendMovies() {
-    return axios.get(`http://api.themoviedb.org/3/trending/movie/week?api_key=a4de692f1b0678dfae28764090f39212`)
+    return axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=a4de692f1b0678dfae28764090f39212`)
         .then(res => res)
 }
 
 export function fetchMoviesDetails(movieId) {
-    return axios.get(`http://api.themoviedb.org/3/movie/${movieId}?api_key=a4de692f1b0678dfae28764090f39212`)
+    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=a4de692f1b0678dfae28764090f39212`)
         .then(res => res)
 }
 
@@ -20,11 +20,11 @@ export function fetchMoviesByQuery(inputValue) {
 }
 
 export function fetchMoviesCast(movieId) {
-    return axios.get(`http://api.themoviedb.org/3/movie/${movieId}/credits?api_key=a4de692f1b0678dfae28764090f39212`)
+    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=a4de692f1b0678dfae28764090f39212`)
 }
 
 export function fetchMoviesReviews(movieId) {
-    return axios.get(`http://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=a4de692f1b0678dfae28764090f39212`)
+    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=a4de692f1b0678dfae28764090f39212`)
 }
 
 
